b83764b28e1f41d06f05cbcb0c9db71c7a300e22
Pratik Bhattacharya <patrickb404@outlook.com>
2017-01-25 11:21:09 +0530

    Restructured folder to support main site Added basic UI component and fremework code

---
+++ Source/App/Ecosters.App/Web/Properties/PublishProfiles/ecosters-main-dev - Web Deploy-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'e78f0454-51a3-41c0-aef8-1438d4dfa24e'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
